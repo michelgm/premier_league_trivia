96bde72a967327f1600a6ada5d8621dcf5890142
michelgm <99933941+michelgm@users.noreply.github.com>
2023-09-13 22:39:56 -0300

    initial screen

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:premier_league_trivia/screens/starting_screen.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: StartingScreen(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
