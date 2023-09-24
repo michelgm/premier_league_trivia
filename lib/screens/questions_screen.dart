63bdd8e4feaeb5910dddce5da8f2ec65c53330a3
michelgm <99933941+michelgm@users.noreply.github.com>
2023-09-23 22:13:57 -0300

    results page

--- lib/screens/questions_screen.dart
+++ lib/screens/questions_screen.dart
@@ -2,18 +2,49 @@ import 'package:flutter/material.dart';
 
 import 'package:premier_league_trivia/widgets/answer_button.dart';
 import 'package:premier_league_trivia/data/questions.dart';
+import '../models/quiz_question.dart';
 
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key});
+  const QuestionsScreen({
+    super.key,
+    required this.onSelectAnswer,
+  });
+
+  final void Function(String answer) onSelectAnswer;
 
   @override
   State<QuestionsScreen> createState() => _QuestionsScreenState();
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  int currentQuestionIndex = 0;
+  late List<QuizQuestion> shuffledQuestions;
+
+  @override
+  void initState() {
+    super.initState();
+    shuffledQuestions = [...questions];
+    shuffledQuestions.shuffle();
+  }
+
+  void answerQuestion(String selectedAnswer) {
+    widget.onSelectAnswer(selectedAnswer);
+    setState(() {
+      if (currentQuestionIndex < shuffledQuestions.length - 1) {
+        currentQuestionIndex++;
+      } else {
+        // Handle end of questions or quiz completion
+        // You can navigate to a result screen or restart the quiz here.
+        // For simplicity, we'll just loop back to the first question.
+        currentQuestionIndex = 0;
+        shuffledQuestions.shuffle();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = shuffledQuestions[currentQuestionIndex];
 
     return SizedBox(
       width: double.infinity,
@@ -34,7 +65,12 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             const SizedBox(height: 30),
             ...currentQuestion.getShuffledAnswers().map(
               (answer) {
-                return AnswerButton(answerText: answer, onTap: () {});
+                return AnswerButton(
+                  answerText: answer,
+                  onTap: () {
+                    answerQuestion(answer);
+                  },
+                );
               },
             ),
           ],
--- lib/widgets/quiz.dart
+++ lib/widgets/quiz.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:premier_league_trivia/data/questions.dart';
 import 'package:premier_league_trivia/screens/questions_screen.dart';
+import 'package:premier_league_trivia/screens/results_screen.dart';
 import 'package:premier_league_trivia/screens/start_screen.dart';
 
 class Quiz extends StatefulWidget {
@@ -10,6 +12,7 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
   var activeScreen = 'start-screen';
 
   void switchScreen() {
@@ -18,13 +21,32 @@ class _QuizState extends State<Quiz> {
     });
   }
 
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'result-screen';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget screenWidget = StartScreen(switchScreen);
 
     if (activeScreen == 'questions-screen') {
-      screenWidget = const QuestionsScreen();
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: chooseAnswer,
+      );
     }
+
+    if (activeScreen == 'result-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+      );
+    }
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
